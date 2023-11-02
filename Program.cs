6ec56cec25920006725f4de70dd4ea0f9673d034
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-02 22:15:34 +0100

    init project structure with todos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace fitnessSheetsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fitness Tracker *****");
+
+            //TODO Optionally: Make this app to track learning process instead of training
+
+            //TODO: Add Menu to choose what kind of record to create
+            //TODO: Add That record to CSV file
+            //TODO: Create some kind of endpoint so Google Sheets training sheet could fetch records and add tables inside
+        }
+    }
+}
